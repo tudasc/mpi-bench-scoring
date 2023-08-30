a4696d221bfbbe316583367d0556ee1da5bced76
Tim Jammer <tim.jammer@tu-darmstadt.de>
2023-08-30 10:08:12 +0200

    Update collect_static_data.sh
    update commit hash to the correct one used

--- mpi-bugs-initiative/collect_static_data.sh
+++ mpi-bugs-initiative/collect_static_data.sh
@@ -11,7 +11,7 @@ cd $MPI_ARG_USAGE_BASE
 
 # set up code locations file
 echo "Code,Language,Type,URL,Comment,SHA" > src_location.csv
-echo "MpiBugsInitiative,C,git,https://gitlab.com/MpiBugsInitiative/MpiBugsInitiative.git,,34daf22e18e89e527a2c7b60fcd0579f1917abec" >> src_location.csv
+echo "MpiBugsInitiative,C,git,https://gitlab.com/MpiBugsInitiative/MpiBugsInitiative.git,,4ec1c8c4e66b264ebe44d74cbd9c353dcbd6b876" >> src_location.csv
 
 
 python3 mpi_usage_analysis.py --output $MPI_ARG_TRACE_DIR/MBI-Static.csv --code_locations src_location.csv --repo_path $MBI_BASE/..
